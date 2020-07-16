d4b55cde4258d98abb8f048f41990921d7347f06
wandalen <wandalen.me@gmail.com>
2020-07-16 16:49:52 +0300

    .

--- proto/wtools/amid/l3/yaml.test/Yaml.test.s
+++ proto/wtools/amid/l3/yaml.test/Yaml.test.s
@@ -13,8 +13,8 @@ if( typeof module !== 'undefined' )
 
 }
 
-var _global = _global_;
-var _ = _global_.wTools;
+let _global = _global_;
+let _ = _global_.wTools;
 
 // --
 // tests
@@ -33,7 +33,7 @@ function routinesOfYamlTools( test )
 // declare
 // --
 
-var Self =
+let Self =
 {
 
   name : 'Tools.mid.l3.yaml',
